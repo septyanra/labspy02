5df299fbe953422783a8b3f2c874d3021e640997
septyanra <57028466+septyanra@users.noreply.github.com>
2019-10-30 00:05:13 -0500

    Add files via upload

---
+++ Python02.py
@@ -0,0 +1,17 @@
+#Data Tinggi Badan Mahasiswa
+
+A = int(input("Data tinggi badan mahasiswa 1 : "))
+B = int(input("Data tinggi badan mahasiswa 2 : "))
+C = int(input("Data tinggi badan mahasiswa 3 : "))
+
+
+
+if A > B and A > C:
+    print("Data tinggi badan tertinggi adalah: ", A)
+if B > A and B > C:
+    print("Data tinggi badan tertinggi adalah: ", B)
+if C > A and C > B:
+    print("Data tinggi badan tertinggi adalah: ", C)
+
+
+
